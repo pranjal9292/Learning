db79455e95d6e353fe853640c8a4e9c000d328bd
Pranjal Thakur <pranjalthakur101@gmail.com>
2020-09-26 12:53:52 +0530

    Add basic sum programme

---
+++ LearningJava/src/assignments/Sum.java
@@ -0,0 +1,12 @@
+package assignments;
+
+public class Sum {
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 0; i < 100; i++) {
+			sum += i;
+		}
+		System.out.println("Sum is " + sum);
+	}
+
+}
