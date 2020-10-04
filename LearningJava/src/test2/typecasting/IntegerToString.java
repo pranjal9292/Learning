61c734bd76bf3c3f9c6003e842722e593ffc7547
Pranjal Thakur <pranjalthakur101@gmail.com>
2020-10-04 12:18:29 +0530

    Typecasting

---
+++ LearningJava/src/test2/typecasting/IntegerToString.java
@@ -0,0 +1,11 @@
+package test2.typecasting;
+
+public class IntegerToString {
+	public static void main(String[] args) {
+
+		String a = "3848";
+		String b = "22";
+		int num = Integer.parseInt(a + b);
+		System.out.print(num);
+	}
+}
