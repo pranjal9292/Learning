03537e337c8ba54ceedb9bf7c9011acfac88b302
Pranjal Thakur <pranjalthakur101@gmail.com>
2020-10-04 11:50:54 +0530

    Test 2

--- LearningJava/src/test2/ZPattern.java
+++ LearningJava/src/test2/ZPattern.java
@@ -4,8 +4,8 @@ public class ZPattern {
 	public static void main(String[] args) {
 		int i, j;
 		for (i = 0; i < 11; i++) {
-			for (j = i; j < 11; j++) {
-				if (i == 0 || i == 10 || j == 10 - i) {
+			for (j = 0; j < 11; j++) {
+				if (i == 0 || i == 10 || j == i || j == 10 - i ) {
 					System.out.print("*");
 				} else {
 					System.out.print(" ");
--- LearningJava/src/test2/abstraction/Main.java
+++ LearningJava/src/test2/abstraction/Main.java
@@ -2,5 +2,8 @@ package test2.abstraction;
 
 public class Main {
 	public static void main(String args[]) {
+		SumAbstractClass sum = new Implementation();
+				System.out.print(sum.sum(423,4));
+				
 	}
 }
